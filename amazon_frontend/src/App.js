d75c019a13add6f8356bf5e9ed631bdb917aba30
Himanshu tyagi <himanshutyagi0158@gmail.com>
2023-02-24 20:10:46 +0530

    finalised work on cart and payment page

--- amazon_frontend/src/App.js
+++ amazon_frontend/src/App.js
@@ -1,22 +1,19 @@
-
 import './App.css';
-import { Cart } from './Pages/Cart_&_Payment/Cart';
 import { Footer } from './Components/Footer';
-
 import Navbar from './Components/Navbar';
 import Dashboard from './Dashboard_Nav/Dashboard';
 import { AllRoutes } from './Routes/AllRoutes';
+import { Payment } from './Pages/Cart_&_Payment/Payment';
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Dashboard />
-      <AllRoutes/>
-        <Cart/>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <Dashboard />
+            <AllRoutes />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- amazon_frontend/src/Routes/AllRoutes.jsx
+++ amazon_frontend/src/Routes/AllRoutes.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { Home } from '../Components/Home'
+import Home from '../Components/Home'
 import Appliancaes from '../Dashboard_Nav/Pages/Appliances'
 import Laptop from '../Dashboard_Nav/Pages/Laptop'
 import Soundbar from '../Dashboard_Nav/Pages/Soundbar'
 import Television from '../Dashboard_Nav/Pages/Television'
 import Watch from '../Dashboard_Nav/Pages/Watch'
+import { Cart } from '../Pages/Cart_&_Payment/Cart'
+import { Payment } from '../Pages/Cart_&_Payment/Payment'
 import { Location } from '../Pages/Location'
 
 export const AllRoutes = () => {
-  return (
-    <div>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-
-     <Route path="/location" element={<Location/>}/>
-     <Route path="/laptop" element={<Laptop/>}/>
-     <Route path="/television" element={<Television/>}/>
-     <Route path="/appliances" element={<Appliancaes/>}/>
-     <Route path="/watch" element={<Watch/>}/>
-     <Route path="/soundbar" element={<Soundbar/>}/>
-    </Routes>
-    </div>
-  )
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/location" element={<Location />} />
+                <Route path="/laptop" element={<Laptop />} />
+                <Route path="/television" element={<Television />} />
+                <Route path="/appliances" element={<Appliancaes />} />
+                <Route path="/watch" element={<Watch />} />
+                <Route path="/soundbar" element={<Soundbar />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/pay" element={<Payment />} />
+                <Route path="*" element={<h1>404 Page not found</h1>} />
+            </Routes>
+        </div>
+    )
 }
