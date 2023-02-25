f97e39aebe0cb82951fa5f0f3c60d14b7f9b67de
saurabhpawar1997 <saurabhpawarrrr@gmail.com>
2023-02-25 23:56:05 +0530

    sp

--- amazon_frontend/src/App.js
+++ amazon_frontend/src/App.js
@@ -1,31 +1,10 @@
-// import Homepage from "./Components/Home/Homepage"
-import Navbar from "./Components/Navbar"
-import {Footer} from "./Components/Footer"
-import Dashboard from "./Dashboard_Nav/Dashboard";
-import { AllRoutes } from "./Routes/AllRoutes";
+
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <Navbar/>
-      <Dashboard/>
-      <AllRoutes/>
-      <Footer/>
+      
     </div>
   );
 }
